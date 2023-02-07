d97875c64415fb112745faecf2e78b46c7ed2ae4
Sangodare Isaac <sangodareisaac92@gmail.com>
2023-02-07 15:56:19 +0100

    user app update with CRUD

--- product/urls.py
+++ product/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from . import views
+from user import views as user_views
 
 from rest_framework.routers import DefaultRouter
 
@@ -7,6 +8,8 @@ from rest_framework.routers import DefaultRouter
 router = DefaultRouter()
 router.register("products", views.ProductsViewSet)
 router.register("categories", views.CategoryViewSet)
+router.register('user', user_views.UserViewSet)
+
 
 urlpatterns = router.urls
 
